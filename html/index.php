eecd5722e447b0220274af7eb550120106444cef
Maksim Semeev <makssemeev@gmail.com>
2022-01-10 21:16:42 +0300

    Added form handler

--- html/index.php
+++ html/index.php
@@ -1,8 +1,24 @@
 <?
 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Welcome to PlayersRate!</title>
+<style>
+</style>
+</head>
+<body>
+
+<?
 echo "<ul>";
 echo "<li><a href=/test.php><b>TEST.PHP</b></a></li>";
-
 echo "</ul>";
-
 ?>
+
+<form action="welcome.php" method="post">
+    Name: <input type="text" name="name"><br>
+    E-mail: <input type="text" name="email"><br>
+    <input type="submit">
+</form>
+
