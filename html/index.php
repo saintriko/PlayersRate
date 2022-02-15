5f53a3e327d674ab327aca9331675c72ae864d98
Maksim Semeev <makssemeev@gmail.com>
2022-02-16 00:08:22 +0300

    transferred "welcome form"

--- html/index.php
+++ html/index.php
@@ -13,12 +13,10 @@
 <?
 echo "<ul>";
 echo "<li><a href=/test.php><b>TEST.PHP</b></a></li>";
+echo "<li><a href=/welcome.php><b>WELCOME.PHP</b></a></li>";
 echo "</ul>";
 ?>
 
-<form action="welcome.php" method="post">
-    Name: <input type="text" name="name"><br>
-    E-mail: <input type="text" name="email"><br>
-    <input type="submit">
-</form>
+
+
 
--- html/welcome.php
+++ html/welcome.php
@@ -1,7 +1,16 @@
-<?php
+<form action="welcome.php" method="get">
+    Name: <input type="text" name="name"><br>
+    E-mail: <input type="text" name="email"><br>
+    <input type="submit">
+</form>
 
+<?
+if (!empty($_GET) && !empty($_GET['name']) && !empty($_GET['email']) ) { 
 ?>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
 
+<?
+}
+?>
