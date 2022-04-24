ece29d8d2f159cc6e730b41cb50c5d2fe0318542
Maksim Semeev <makssemeev@gmail.com>
2022-04-24 22:43:23 +0300

    added Doctrine and Product Entity

---
+++ html/scripts/show_all_products.php
@@ -0,0 +1,16 @@
+<?php
+// show_product.php <id>
+require_once "../bootstrap.php";
+
+$productRepository = $entityManager->getRepository('Product');
+$products = $productRepository->findAll();
+
+if ($products === null) {
+    echo "No products found.\n";
+    exit(1);
+} else {
+    foreach ($products as $product) {
+        echo $product->getId() . " " . $product->getName() . " " . $product->getModel() . "\n";
+        echo '<br>';
+    }
+}
