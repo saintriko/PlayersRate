f8b6ac9d5df83b3f1834b464a7646a4bf38f2903
Maksim Semeev <makssemeev@gmail.com>
2022-04-25 22:18:13 +0300

    Added phpdotenv

--- html/bootstrap.php
+++ html/bootstrap.php
@@ -5,23 +5,26 @@ use Doctrine\ORM\EntityManager;
 
 require_once "vendor/autoload.php";
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 // Create a simple "default" Doctrine ORM configuration for Annotations
 $isDevMode = true;
 $proxyDir = null;
 $cache = null;
 $useSimpleAnnotationReader = false;
-$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src"), $isDevMode, $proxyDir, $cache, $useSimpleAnnotationReader);
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__ ."/src/entities"), $isDevMode, $proxyDir, $cache, $useSimpleAnnotationReader);
 // or if you prefer yaml or XML
 //$config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"), $isDevMode);
 //$config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
 
 // database configuration parameters
 $conn = array(
-    'dbname' => 'my_db',
-    'user' => 'root',
-    'password' => '1234',
-    'host' => 'localhost',
-    'driver' => 'pdo_mysql',
+    'dbname' => $_ENV['DATABASE_NAME'],
+    'user' => $_ENV['DATABASE_USER'],
+    'password' => $_ENV['DATABASE_PASSWORD'],
+    'host' => $_ENV['DATABASE_HOST'],
+    'driver' => $_ENV['DATABASE_DRIVER'],
 );
 
 // obtaining the entity manager
--- html/scripts/all_products_JSON.php
+++ html/scripts/all_products_JSON.php
@@ -1,12 +1,13 @@
 <?
 header('Content-Type: application/json; charset=utf-8');
 require_once "../bootstrap.php";
+
 $productsData = [];
 $productRepository = $entityManager->getRepository('Product');
 $products = $productRepository->findAll();
 
 if ($products === null) {
-    echo "No products found.\n";
+    echo json_encode("No products found.");
     exit(1);
 } else {
     echo json_encode($products);
